f273415c1fdfe3a16a592055291c4e387d9e70df
Cosmin <cosmin.codrea@flipsidegroup.com>
2022-06-10 17:37:00 +0300

    remove preview from main activity

--- app/src/main/java/com/example/composetemplate/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composetemplate/presentation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.composetemplate.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetemplate.utils.theme.ComposeTemplateTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,14 +16,5 @@ class MainActivity : ComponentActivity() {
                 DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTemplateTheme {
-//        CoinListScreen()
     }
 }
